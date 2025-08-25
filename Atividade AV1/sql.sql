60f2df03aeb9bf6a745b179188b639eaef30b764
Gabriel <gabrielfurtunato101@gmail.com>
2025-08-25 16:44:11 -0300

    AV1

---
+++ Atividade AV1/sql.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE AV1;
+
+USE AV1;
+
+CREATE TABLE Fornecedor (
+fcodigo int auto_increment primary key not null,
+fnome varchar(50) not null,
+fstatus int not null,
+cidade varchar(30) not null
+);
+
+CREATE TABLE Peca (
+pcodigo int auto_increment primary key not null,
+pnome varchar(50) not null,
+cor varchar(50),
+peso int,
+cidade varchar(30)
+);
+
+CREATE TABLE Instituicao (
+icodigo int auto_increment primary key not null,
+nome varchar(50) not null
+);
+
+CREATE TABLE Projeto (
+prcod int auto_increment primary key not null,
+prnome varchar(50) not null,
+cidade varchar(30) not null
+);
+
+create table Fornecimento (
+ffor int auto_increment primary key not null,
+pcod int,
+fcod int,
+foreign key (pcod) references Peca (pcod),
+foreign key (fcod) references Fornecedor (fcod)
+);
+
+ALTER TABLE Fornecedor
+change column fcodigo fcod int auto_increment not null;
+
+ALTER TABLE Peca
+change column pcodigo pcod int auto_increment not null;
+
+
